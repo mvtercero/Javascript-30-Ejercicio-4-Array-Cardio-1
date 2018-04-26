16e69c4727977981efa9302809ab0f75d0909484
mvtercero <mvtercero85@gmail.com>
2018-04-26 09:40:56 +0200

    Add method map

--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@
         { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
         { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
       ];
+      
       const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'];
       // Array.prototype.filter()
       // 1. Filter the list of inventors for those who were born in the 1500's
@@ -25,11 +26,15 @@
 
       console.table(fifteen);
 
-
-
-
       // Array.prototype.map()
       // 2. Give us an array of the inventors' first and last names
+
+      //const fullName = inventors.map(inventor=> (inventor.first + " " + inventor.last))
+      const fullName = inventors.map(inventor=>`${inventor.first} ${inventor.last}`)
+      console.table(fullName);
+
+
+
       // Array.prototype.sort()
       // 3. Sort the inventors by birthdate, oldest to youngest
       // Array.prototype.reduce()
